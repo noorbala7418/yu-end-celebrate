9923d108101baab47dfef746e4e13f4f1ccab948
Mahdi Noorbala <noorbala7418@gmail.com>
2021-10-07 02:27:38 +0330

    add realtion students for excel file

--- app/Models/Anjoman.php
+++ app/Models/Anjoman.php
@@ -10,7 +10,19 @@ class Anjoman extends Model
     use HasFactory;
 
     protected $table = 'anjomans';
+    
     protected $fillable = [
         'name', 'person_price', 'hamrahan_price', 'total_people', 'used_people'
     ];
+
+    
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'anjoman_id', 'id');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'anjoman_id', 'id');
+    }
 }
